6cf3557c549678fcbab330105de623f5a074f3b5
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-06-19 08:42:35 +0900

    データの抽出

--- myapp.sql
+++ myapp.sql
@@ -5,12 +5,17 @@ create table users (
   score float
 );
 
-alter table users add column email varchar(255) after name;
-alter table users drop column score;
-alter table users change name user_name varchar(80) default 'nobody';
+insert into users (name, score) values ('taguchi', 5.8);
+insert into users (name, score) values ('fkoji', 8.2);
+insert into users (name, score) values ('dotinstall', 6.1);
+insert into users (name, score) values ('Tanaka', 4.2);
+insert into users (name, score) values ('yamada', null);
+insert into users (name, score) values ('tashiro', 7.9);
 
-desc users;
-
-alter table users rename persons;
-show tables;
-drop table if exists persons;
+-- select * from users;
+-- select id, name from users;
+-- select * from users where score >= 6.0;
+-- select * from users where score >= 3.0 and score <= 6.0;
+-- select * from users where score between 3.0 and 6.0;
+select * from users where name = 'taguchi' or name = 'fkoji';
+select * from users where name in ('taguchi', 'fkoji');
